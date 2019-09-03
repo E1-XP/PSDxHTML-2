fcbe794bf2435bc67fbf2f42ae5ff9d2d8cca329
E!-XP <poczta.elektroniczna@hotmail.com>
2019-09-03 19:49:24 +0200

    add remaining sections

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,45 @@
-const mainNav = document.getElementById("main-nav");
-const navList = document.getElementById("nav-list");
-const navButton = document.getElementById("nav-btn");
+import Siema from 'siema';
+import initMap from './map';
 
-const isNavOpen = () => mainNav.classList.contains("js-is-open");
+const mainNav = document.getElementById('main-nav');
+const navList = document.getElementById('nav-list');
+const navButton = document.getElementById('nav-btn');
+const carouselBtns = document.querySelectorAll('.carousel .carousel__button');
+
+const isNavOpen = () => mainNav.classList.contains('js-is-open');
 const blockScroll = () => window.scrollTo(0, 0);
-const enableTransition = () => navList.classList.remove("js-transition-none");
+const enableTransition = () => navList.classList.remove('js-transition-none');
 const disableTransition = () => {
-  if (!isNavOpen()) navList.classList.add("js-transition-none");
+  if (!isNavOpen()) navList.classList.add('js-transition-none');
 };
 
 const onNavClick = () => {
-  mainNav.classList.toggle("js-is-open");
+  mainNav.classList.toggle('js-is-open');
   if (isNavOpen()) enableTransition();
-  navButton.setAttribute("aria-expanded", `${isNavOpen()}`);
+  navButton.setAttribute('aria-expanded', `${isNavOpen()}`);
 
-  const methods = ["removeEventListener", "addEventListener"];
-  window[methods[+isNavOpen()]]("scroll", blockScroll);
+  const methods = ['removeEventListener', 'addEventListener'];
+  window[methods[+isNavOpen()]]('scroll', blockScroll);
 };
 
-navButton.addEventListener("click", onNavClick);
-navList.addEventListener("transitionend", disableTransition);
+navButton.addEventListener('click', onNavClick);
+navList.addEventListener('transitionend', disableTransition);
+
+const updateSliderControls = idx => {
+  carouselBtns.forEach(el => el.classList.remove('carousel__button--active'));
+  carouselBtns[idx].classList.add('carousel__button--active');
+};
+
+const carousel = new Siema({
+  selector: '.carousel .carousel__list',
+  loop: true,
+  duration: 300,
+  onChange: () => updateSliderControls(carousel.currentSlide)
+});
+
+carouselBtns.forEach((elem, i) =>
+  elem.addEventListener('click', () => {
+    carousel.goTo(i);
+    updateSliderControls(i);
+  })
+);
