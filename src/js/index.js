ddc55dd6662abb9f12efdbbb9274b204f9d18c02
E!-XP <poczta.elektroniczna@hotmail.com>
2019-09-05 10:44:20 +0200

    work on gallery, add anchor nav

--- src/js/index.js
+++ src/js/index.js
@@ -4,42 +4,127 @@ import initMap from './map';
 const mainNav = document.getElementById('main-nav');
 const navList = document.getElementById('nav-list');
 const navButton = document.getElementById('nav-btn');
-const carouselBtns = document.querySelectorAll('.carousel .carousel__button');
+const lightbox = document.querySelector('#gallery #lightbox');
+const lightboxBtn = lightbox.querySelector('.carousel__toggle-button');
+const galleryItems = document.querySelectorAll(
+  '#gallery .section-gallery__item'
+);
+const lightboxBtns = lightbox.querySelectorAll('.carousel__button');
+const carouselBtns = document.querySelectorAll(
+  '#testimonials .carousel__button'
+);
 
 const isNavOpen = () => mainNav.classList.contains('js-is-open');
 const blockScroll = () => window.scrollTo(0, 0);
-const enableTransition = () => navList.classList.remove('js-transition-none');
-const disableTransition = () => {
-  if (!isNavOpen()) navList.classList.add('js-transition-none');
+const enableTransition = elem => elem.classList.remove('h-transition-none');
+const disableTransition = (elem, predicate = true) => {
+  const should = typeof predicate === 'function' ? predicate() : predicate;
+  if (should) elem.classList.add('h-transition-none');
 };
 
 const onNavClick = () => {
   mainNav.classList.toggle('js-is-open');
-  if (isNavOpen()) enableTransition();
-  navButton.setAttribute('aria-expanded', `${isNavOpen()}`);
+  const isOpen = isNavOpen();
+  if (isOpen) enableTransition(navList);
+  navButton.setAttribute('aria-expanded', `${isOpen}`);
 
   const methods = ['removeEventListener', 'addEventListener'];
-  window[methods[+isNavOpen()]]('scroll', blockScroll);
+  window[methods[+isOpen]]('scroll', blockScroll);
 };
 
 navButton.addEventListener('click', onNavClick);
-navList.addEventListener('transitionend', disableTransition);
+navList.addEventListener('transitionend', () => disableTransition(navList));
 
-const updateSliderControls = idx => {
-  carouselBtns.forEach(el => el.classList.remove('carousel__button--active'));
-  carouselBtns[idx].classList.add('carousel__button--active');
-};
+document.querySelectorAll('a[href^="#"]').forEach(item => {
+  item.addEventListener('click', e => {
+    e.preventDefault();
 
-const carousel = new Siema({
-  selector: '.carousel .carousel__list',
-  loop: true,
-  duration: 300,
-  onChange: () => updateSliderControls(carousel.currentSlide)
+    if (navList.contains(item) && isNavOpen()) onNavClick();
+    document.querySelector(item.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
 });
 
-carouselBtns.forEach((elem, i) =>
-  elem.addEventListener('click', () => {
-    carousel.goTo(i);
-    updateSliderControls(i);
-  })
-);
+const updateSliderControls = (idx, elemArr) => {
+  elemArr.forEach(el => el.classList.remove('carousel__button--active'));
+  elemArr[idx].classList.add('carousel__button--active');
+};
+
+const lightboxSlider = new Siema({
+  selector: '#gallery .carousel--lightbox .carousel__list',
+  loop: true,
+  duration: 300,
+  onChange: () =>
+    updateSliderControls(lightboxSlider.currentSlide, lightboxBtns)
+});
+
+const handleKeyDown = e => {
+  switch (e.which) {
+    case 27:
+      return toggleLightbox();
+    case 37:
+      return lightboxSlider.prev();
+    case 39:
+      return lightboxSlider.next();
+    default:
+      return null;
+  }
+};
+
+const toggleLightbox = (evt, idx) => {
+  const methods = ['removeEventListener', 'addEventListener'];
+
+  const toggleLightboxKboardCtrl = isOpen => {
+    document[methods[+isOpen]]('keydown', handleKeyDown);
+  };
+  const isClosed = lightbox.classList.contains('h-hidden-with-transition');
+
+  const toggleVisibility = () =>
+    lightbox.classList.toggle('h-hidden-with-transition');
+  const waitForTransition = fn => setTimeout(fn, 200);
+
+  if (isClosed && idx !== undefined) {
+    enableTransition(lightbox);
+
+    if (lightboxSlider.currentSlide !== idx) {
+      lightboxSlider.goTo(idx, () => waitForTransition(toggleVisibility));
+    } else waitForTransition(toggleVisibility);
+
+    waitForTransition(() => toggleLightboxKboardCtrl(isClosed));
+  } else {
+    const disableTransitionOffEvt = () => {
+      disableTransition(lightbox);
+      lightbox[methods[0]]('transitionend', disableTransitionOffEvt);
+    };
+
+    toggleLightboxKboardCtrl(isClosed);
+    lightbox[methods[1]]('transitionend', disableTransitionOffEvt);
+    toggleVisibility();
+  }
+};
+
+galleryItems.forEach((item, idx) => {
+  item.addEventListener('click', () => toggleLightbox(undefined, idx));
+});
+lightboxBtn.addEventListener('click', toggleLightbox);
+
+const testimonialsCarousel = new Siema({
+  selector: '#testimonials .carousel__list',
+  loop: true,
+  duration: 300,
+  onChange: () =>
+    updateSliderControls(testimonialsCarousel.currentSlide, carouselBtns)
+});
+
+const enableCarouselBtns = (carousel, btns) => {
+  btns.forEach((elem, i) =>
+    elem.addEventListener('click', () => {
+      carousel.goTo(i);
+      updateSliderControls(i, btns);
+    })
+  );
+};
+
+enableCarouselBtns(lightboxSlider, lightboxBtns);
+enableCarouselBtns(testimonialsCarousel, carouselBtns);
